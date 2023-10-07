f518e2c39f38f1a5c765dc4063f7b7cc72f05a0c
MorenaJv28 <124007217+MorenaJv28@users.noreply.github.com>
2023-10-07 16:09:32 -0600

    Ejercicio 2

---
+++ Ejercicio 2/Ejercicio 2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Ejercicio 2
+Console.WriteLine(@"Dada la fecha actual extraer, el día, mes, año, y decir cuántos días faltan para
+navidad y año nuevo.");
+
+DateTime fechaActual = DateTime.Now;
+
+    DateTime Navidad = new DateTime(fechaActual.Year, 12, 25);
+    DateTime AñoNuevo = new DateTime(fechaActual.Year + 1, 1, 1);
+
+    int diasParaNavidad = (Navidad - fechaActual).Days;
+    int diasParaAñoNuevo = (AñoNuevo - fechaActual).Days;
+    Console.WriteLine();
+    Console.WriteLine($" Para Navidad restan {diasParaNavidad} dias");
+    Console.WriteLine();
+    Console.WriteLine($" Para Año Nuevo restan : {diasParaAñoNuevo} dias");
+
+Console.WriteLine("Ing. Gutierrez");
+
