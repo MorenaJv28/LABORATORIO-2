4b130d0b32b55f762a928d92d8bbf64062a83816
MorenaJv28 <124007217+MorenaJv28@users.noreply.github.com>
2023-10-07 15:43:30 -0600

    Ejercicio de calculadora

---
+++ Laboratorio 2/Laboratorio 2/Program.cs
@@ -0,0 +1,35 @@
+﻿//Ejercicio 1
+Console.WriteLine(@"1. Pedir las coordenadas del punto 1 (x, y) y radio en el centro (h, k) y calcular la
+distancia");
+
+Console.Write("Bienvenido a su calculadora");
+Console.Write("============================");
+
+Console.Write("Introduce la coordenada x: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Introduce la coordenada y: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Introduce la coordenada h: ");
+double h = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Introduce la coordenada k: ");
+double k = Convert.ToDouble(Console.ReadLine());
+
+double R = Math.Sqrt(Math.Pow(h - x1, 2) + Math.Pow(k - y1, 2));
+
+Console.WriteLine($"La distancia de los puntos P({x1}, {y1}) y Q({h}, {k}) es: {R}");
+
+
+
+
+
+
+
+
+
+
+
+
+
